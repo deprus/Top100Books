315c3d4ebe928b2caa374c1796625f57fb8af816
deprus <deprus97@gmail.com>
2023-12-14 23:29:55 +0300

    fix: cors origin

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,7 @@ export const prisma = new PrismaClient();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://top100books-backend-deprus.vercel.app/",
+    origin: "https://top100books-frontend.vercel.app",
     credentials: true,
   })
 );
