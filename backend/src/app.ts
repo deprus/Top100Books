198f224d9172ba273356e984010c727bfbe7027a
deprus <deprus97@gmail.com>
2023-12-14 22:53:33 +0300

    try simple route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,8 +12,8 @@ export const prisma = new PrismaClient();
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(cookieParser());
-app.use("/api/books", bookRouter);
-app.use("/api/auth", authRouter);
+// app.use("/api/books", bookRouter);
+// app.use("/api/auth", authRouter);
 
 app.get("/", (_req: Request, res: Response) => {
   res.send("Server is running...");
